a465f02d377eed55a759da4bf3821b030bd37315
Denis Galeev <dmdenis74chel@yandex.ru>
2024-08-03 01:22:36 +0500

    2nd Commit_before_10_Sprint

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,3 +1,7 @@
+# Импортируем настройки проекта.
+from django.conf import settings  # type: ignore
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static  # type: ignore
 from django.contrib import admin  # type: ignore
 from django.urls import include, path  # type: ignore
 
@@ -5,4 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
